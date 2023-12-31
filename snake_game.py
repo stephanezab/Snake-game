a7395fbd97f5533656626ab4a01451296fa48a99
stephanezab <stephanezab@gmail.com>
2023-12-30 19:27:16 -0500

    Update snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -76,9 +76,9 @@ def gameLoop():  # main game loop
             pygame.display.update()
 
             for event in pygame.event.get():
-                # if event.type == pygame.QUIT:
-                #     game_over = True
-                #     game_close = False
+                if event.type == pygame.QUIT:
+                    game_over = True
+                    game_close = False
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_q:
                         game_over = True
