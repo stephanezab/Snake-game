d8adf2aed6ba6366c3db7d589f76d0fa8d6a9a50
stephanezab <stephanezab@gmail.com>
2024-01-02 17:29:47 -0500

    Update snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -5,12 +5,9 @@ import random
 pygame.init()
 
 # Define colors
-white = (255, 255, 255)
-yellow = (255, 255, 102)
-black = (0, 0, 0)
-red = (213, 50, 80)
-green = (0, 255, 0)
-blue = (50, 153, 213)
+red = "#e71c15"
+green = "#30d109"
+blue = "#040673"
 
 # Display size
 WIN_WIDTH = 600
@@ -18,7 +15,7 @@ WIN_HEIGHT = 400
 
 # Initialize display
 WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-pygame.display.set_caption('Snake Game by Pythonista')
+pygame.display.set_caption('Snake Game')
 
 clock = pygame.time.Clock()
 
@@ -39,11 +36,11 @@ score_font = pygame.font.SysFont(None, 25)
 #         pygame.draw.rect(WIN, black, [x[0], x[1], SNAKE_BLOCK, SNAKE_BLOCK])
 
 def draw(snake_list, score):
-    value = score_font.render("Your Score: " + str(score), True, yellow)
+    value = score_font.render("Your Score: " + str(score), True, green)
     WIN.blit(value, [0, 0])
 
     for x in snake_list:
-        pygame.draw.rect(WIN, black, [x[0], x[1], SNAKE_BLOCK, SNAKE_BLOCK])
+        pygame.draw.rect(WIN, green, [x[0], x[1], SNAKE_BLOCK, SNAKE_BLOCK])
 
 # Function to display messages
 def message(msg, color):
@@ -55,7 +52,7 @@ def gameLoop():  # main game loop
     game_close = False
 
     x1 = WIN_WIDTH / 2
-    y1 = WIN_HEIGHT / 2
+    y1 = 0
 
     x1_change = 0
     y1_change = 0
@@ -64,8 +61,8 @@ def gameLoop():  # main game loop
     Length_of_snake = 1
 
     # Randomly place food item
-    foodx = round(random.randrange(0, WIN_WIDTH - SNAKE_BLOCK) / 10.0) * 10.0
-    foody = round(random.randrange(0, WIN_HEIGHT - SNAKE_BLOCK) / 10.0) * 10.0
+    foodx = round(random.randrange(0, WIN_WIDTH - SNAKE_BLOCK) / SNAKE_BLOCK) * SNAKE_BLOCK
+    foody = round(random.randrange(0, WIN_HEIGHT - SNAKE_BLOCK) / SNAKE_BLOCK) * SNAKE_BLOCK
 
     while not game_over:
 
@@ -103,13 +100,13 @@ def gameLoop():  # main game loop
                     y1_change = SNAKE_BLOCK
                     x1_change = 0
 
-        if x1 >= WIN_WIDTH - SNAKE_BLOCK or x1 < 0 or y1 >= WIN_HEIGHT- SNAKE_BLOCK or y1 < 0:
+        if x1 >= WIN_WIDTH  or x1 < 0 or y1 >= WIN_HEIGHT or y1 < 0:
             game_close = True
 
         x1 += x1_change
         y1 += y1_change
         WIN.fill(blue)
-        pygame.draw.rect(WIN, green, [foodx, foody, SNAKE_BLOCK, SNAKE_BLOCK])
+        pygame.draw.rect(WIN, red, [foodx, foody, SNAKE_BLOCK, SNAKE_BLOCK])
         snake_Head = []
         snake_Head.append(x1)
         snake_Head.append(y1)
@@ -130,8 +127,8 @@ def gameLoop():  # main game loop
         pygame.display.update()
 
         if x1 == foodx and y1 == foody:
-            foodx = round(random.randrange(0, WIN_WIDTH - SNAKE_BLOCK) / 10.0) * 10.0
-            foody = round(random.randrange(0, WIN_HEIGHT - SNAKE_BLOCK) / 10.0) * 10.0
+            foodx = round(random.randrange(0, WIN_WIDTH - SNAKE_BLOCK) / SNAKE_BLOCK) * SNAKE_BLOCK
+            foody = round(random.randrange(0, WIN_HEIGHT - SNAKE_BLOCK) / SNAKE_BLOCK) * SNAKE_BLOCK
             Length_of_snake += 1
 
         clock.tick(SNAKE_SPEED)
